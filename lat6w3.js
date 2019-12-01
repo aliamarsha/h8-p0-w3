331d7ad329365316abea94f706b5323550de672f
aliamarsha <41851802+aliamarsha@users.noreply.github.com>
2019-12-01 23:05:23 +0700

    update palindrome angka

--- lat6w3.js
+++ lat6w3.js
@@ -6,6 +6,15 @@ function angkaPalindrome(num) {
     if (length === 1 && num < 9){
         return num + 1; 
     }
+     for (var i = 0; i < length / 2; i++) {
+        if (arr[length - 1 - i] > arr[i]) {
+            arr[length - 1 - i] = arr[i];
+            arr[length - 2 - i] += 1;
+            } else if (arr[length - 1 - i] < arr[i]) {
+                       arr[length - 1 - i] = arr[i];
+             }
+        }
+        return parseInt(arr.join(""));
   }
   
   // TEST CASES
