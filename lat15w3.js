e6af0d4c8039fa2180581d69c4a4b59e343edcbe
aliamarsha <41851802+aliamarsha@users.noreply.github.com>
2019-12-01 23:23:24 +0700

    add exercise 15

---
+++ lat15w3.js
@@ -0,0 +1,11 @@
+//Mengelompokkan Hewan
+
+function groupAnimals(animals) {
+    var result = [[], [], []]; 
+}
+  
+  // TEST CASES
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
