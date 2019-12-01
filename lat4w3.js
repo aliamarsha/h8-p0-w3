4a9a6caa12da630767fe1f20977171f66ae40e07
aliamarsha <41851802+aliamarsha@users.noreply.github.com>
2019-12-01 23:03:18 +0700

    update Menggunakan Built-in Function pada Array

--- lat4w3.js
+++ lat4w3.js
@@ -1,31 +1,36 @@
 //Menggunakan Built-in Function pada Array BELUM SELESAI
 
 function dataHandling2(arr){
-    arr.splice(1, 1, 'Roman Alamsyah Elsharawy') 
-    arr.splice(1, 2, 'Provinsi Bandar Lampung')
+    arr.splice(1, 0, 'Roman Alamsyah Elsharawy') 
+    arr.splice(2, 2, 'Provinsi Bandar Lampung')
+    arr.splice(4, 1, 'Pria', 'SMA Internasional Metro' )
     console.log(arr);
-    var date = arr.split( " ");
-    console.log(date[3]);
+    var date = arr[3].split("/");
+    console.log(date);
+    var dateFormat = date[0] + "-" + date[1] + "-" + date[2]; 
+    console.log(dateFormat);
+    var month = " ";
+    var newDate = date[0] + month + date[2];
 
-    switch (newDate){
-        case 01: { console.log(date[0] + "January" + date[2]); break;}
-        case 02: { console.log(date[0] + "February" + date[2]); break;}
-        case 03: { console.log(date[0] + "March" + date[2]); break;}
-        case 04: { console.log(date[0] + "April" + date[2]); break;}
-        case 05: { console.log(date[0] + "May" + date[2]); break;}
-        case 06: { console.log(date[0] + "June" + date[2]); break;}
-        case 07: { console.log(date[0] + "July" + date[2]); break;}
-        case 08: { console.log(date[0] + "August" + date[2]); break;} 
-        case 09: { console.log(date[0] + "September" + date[2]); break;}
-        case 10: { console.log(date[0] + "October" + date[2]); break;}
-        case 11: { console.log(date[0] + "November" + date[2]); break;}
-        case 12: { console.log(date[0] + "December" + date[2]); break;} 
+    switch (month){
+        case "01": "January";  break; 
+        case "02": "February"; break; 
+        case "03": "March"; break;
+        case "04": "April"; break;
+        case "05": "May"; break;
+        case "06": "June"; break;
+        case "07": "July"; break;
+        case "08": "August"; break;
+        case "09": "September"; break;
+        case "10": "October"; break;
+        case "11": "November"; break;
+        case "12": "December"; break;
         default: 
-            text = "Please enter date";
-}
-console.log(newDate)
+          text = "Please enter date";
+    }
+   return month
 }
 
 
 var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
-dataHandling2(input);
+dataHandling2(input); 
