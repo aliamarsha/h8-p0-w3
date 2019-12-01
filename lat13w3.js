d8f9fd18b7119999252420ff171f1a628de0d5d3
aliamarsha <41851802+aliamarsha@users.noreply.github.com>
2019-12-01 20:42:58 +0700

    update target terdekat

--- lat13w3.js
+++ lat13w3.js
@@ -1,1 +1,26 @@
 // Target Terdekat
+
+function targetTerdekat(arr) {
+    var o = arr.indexOf("o"); 
+    var index = arr.indexOf("x");
+    if (index === -1){
+        return 0; 
+    }
+    var closest = arr.length; 
+    while (index != -1){
+        var space = Math.abs( o - index); 
+        if (space < closest){
+            closest = space; 
+        }
+        index = arr.indexOf('x', index + 1); 
+    }
+    return closest;
+    }
+    
+  
+  // TEST CASES
+  console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+  console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+  console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+  console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+  console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
